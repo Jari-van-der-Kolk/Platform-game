6d7688bb2a0ae7c1fe7ace9fce742ae0e3f87cc1
Jari-van-der-Kolk <jari.vanderkolk@gmail.com>
2021-12-14 00:47:46 +0100

    added behaviour tree and state machine

---
+++ WIP platform game/Assets/Game/Scripts/AI/State machine/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+    public abstract class State
+    {
+        protected EnemyBehaviour enemyBehaviour;
+
+        public State(EnemyBehaviour enemyBehaviour)
+        {
+            this.enemyBehaviour = enemyBehaviour;
+        }
+        
+        public abstract void EnterState();
+
+        public abstract void Update();
+        
+        
+
+    }
---
+++ WIP platform game/Assets/Game/Scripts/AI/State machine/States/Restart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+    public class Restart : State
+    {
+        public Restart(EnemyBehaviour enemyBehaviour) : base(enemyBehaviour) { }
+
+        public override void EnterState()
+        {
+            Debug.Log(3);
+            enemyBehaviour.rb.AddForce(Vector3.up);
+            enemyBehaviour.joint.maxDistance = 0;
+            enemyBehaviour.NewWebLocation(50);
+            enemyBehaviour.StartCoroutine(ToIdle());
+        }
+
+        public override void Update()
+        {
+            enemyBehaviour.IsWebActive(true);
+        }
+
+        private IEnumerator ToIdle()
+        {
+            yield return new WaitForSeconds(4f);
+            enemyBehaviour.SwitchState(new Idle(enemyBehaviour));
+        }
+    }
