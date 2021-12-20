0142adff347d6dd8d3aa17bc075579d4a22a3013
jari-van-der-kolk <jari.vanderkolk@gmail.com>
2021-12-20 23:52:10 +0100

    map

--- WIP platform game/Assets/Game/Scripts/Inventory System/InventorySystem.cs
+++ WIP platform game/Assets/Game/Scripts/Inventory System/InventorySystem.cs
@@ -4,14 +4,66 @@ using UnityEngine;
 
 public class InventorySystem : MonoBehaviour
 {
+    public static InventorySystem instance;
+    
     private Dictionary<InventoryItemData, InventoryItem> _itemDictionary;
     public List<InventoryItem> inventory { get; private set; }
     
     void Awake()
     {
+        Init();
+    }
+
+    public void Init()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }else if (instance != null)
+        {
+            Destroy(this);
+        }
+        
         inventory = new List<InventoryItem>();
         _itemDictionary = new Dictionary<InventoryItemData, InventoryItem>();
     }
+    
+    public InventoryItem Get(InventoryItemData referenceData)
+    {
+        if (_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+        {
+            return value;
+        }
+        return null;
+    }
+    
+    public void Add(InventoryItemData referenceData)
+    {
+        if (_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+        {
+            value.AddToStack();
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(referenceData);
+            inventory.Add(newItem);
+            _itemDictionary.Add(referenceData, newItem);
+        }
+    }
 
+    public void Remove(InventoryItemData referenceData)
+    {
+        if (_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+        {
+            value.RemoveFromStack();
+
+            if (value.stackSize == 0)
+            {
+                inventory.Remove(value);
+                _itemDictionary.Remove(referenceData);
+
+            }
+        }
+    }
     
 }
---
+++ WIP platform game/Assets/Game/Scripts/Inventory System/ItemObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public InventoryItemData referenceItem;
+
+    public List<ItemRequirement> requirements;
+
+    public bool removeRequirementsOnPickup;
+    
+    public void OnHandlePickupItem()
+    {
+        if (MeetsRequirements())
+        {
+            if (removeRequirementsOnPickup)
+            {
+                RemoveRequirements();
+            }
+        }
+        InventorySystem.instance.Add(referenceItem);
+        //deactivate the object you are picking up
+        Destroy(gameObject);
+    }
+
+    private bool MeetsRequirements()
+    {
+        foreach (ItemRequirement requirement in requirements)
+        {
+            if(!requirement.HasRequirement()){return false;}
+        }
+
+        return true;
+    }
+
+    public void RemoveRequirements()
+    {
+        foreach (ItemRequirement requirement in requirements)
+        {
+            for (int i = 0; i < requirement.amount; i++)
+            {
+                InventorySystem.instance.Remove(requirement.itemData);
+            }
+        }
+    }
+}
+
+[Serializable]
+public struct ItemRequirement
+{
+    public InventoryItemData itemData;
+    public int amount;
+
+    public bool HasRequirement()
+    {
+        InventoryItem item = InventorySystem.instance.Get(itemData);
+
+        if (item == null || item.stackSize < amount) { return false; }
+
+        return true;
+    }
+}
